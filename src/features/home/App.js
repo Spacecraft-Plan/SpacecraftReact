0b101c3148b7a18394ae7b49a721628cc7cf25a9
hawks.jamesf <hawks.jamesf@gmail.com>
2019-11-16 23:24:19 +0800

    add firebase

--- src/features/home/App.js
+++ src/features/home/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import * as firebase from "firebase/app";
 
 /*
   This is the root component of your app. Here you define the overall layout
@@ -14,6 +15,27 @@ export default class App extends Component {
   static defaultProps = {
     children: '',
   };
+
+  componentWillMount() {
+    // Initialize Firebase
+    // var firebase = require('firebase/app');
+    var config = {
+      apiKey: 'AIzaSyCgJOEsq12RhD9EOBBnR1VSZNSvCW1Sdtg',
+      authDomain: 'spacecraft-22dc1.firebaseapp.com',
+      databaseURL: 'https://spacecraft-22dc1.firebaseio.com',
+      projectId: 'spacecraft-22dc1',
+      storageBucket: 'spacecraft-22dc1.appspot.com',
+      messagingSenderId: '28459008283',
+    };
+    firebase.initializeApp(config);
+    const perf = firebase.performance();
+    const trace = perf.trace('customTraceName');
+    trace.start();
+
+    // code that you want to trace
+
+    trace.stop();
+  }
 
   render() {
     return (
