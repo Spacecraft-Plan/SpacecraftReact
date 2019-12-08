c5144da57a17a59dc57d359e2db612ebc4db4db0
hawks.jamesf <hawks.jamesf@gmail.com>
2019-12-08 10:32:44 +0800

    add firebase

--- src/features/home/App.js
+++ src/features/home/App.js
@@ -4,6 +4,10 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/performance";
+import "firebase/storage"
+import "firebase/functions"
+import "firebase/messaging"
+import "firebase/database"
 
 /*
   This is the root component of your app. Here you define the overall layout
@@ -20,8 +24,6 @@ export default class App extends Component {
   };
 
   componentWillMount() {
-    // Initialize Firebase
-    // var firebase = require('firebase/app');
     var config = {
       apiKey: 'AIzaSyCgJOEsq12RhD9EOBBnR1VSZNSvCW1Sdtg',
       authDomain: 'spacecraft-22dc1.firebaseapp.com',
@@ -29,6 +31,10 @@ export default class App extends Component {
       projectId: 'spacecraft-22dc1',
       storageBucket: 'spacecraft-22dc1.appspot.com',
       messagingSenderId: '28459008283',
+      // appId: "1:28459008283:web:809f4571433c65f9",
+      // measurementId: "G-83LV95LTW7"
+      appId: "1:28459008283:web:e9305848b03479a1",
+      measurementId: "G-WZ12BEX6RG",
     };
     firebase.initializeApp(config);
     const perf = firebase.performance();
@@ -38,6 +44,18 @@ export default class App extends Component {
     // code that you want to trace
 
     trace.stop();
+    var storageRef = firebase.storage().ref();
+    console.log(storageRef)
+    var image = storageRef.child('WechatIMG168.jpeg')
+    console.log(image.parent + image.name)
+
+    var firestoreRef = firebase.firestore().collection('users').doc('QMqoTvCUnT1Ve2Jek5fL');
+    console.log(firestoreRef)
+
+    var auth = firebase.auth()
+
+    var functions = firebase.functions()
+
   }
 
   render() {
